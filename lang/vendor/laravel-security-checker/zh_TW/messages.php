979ab0a13acd8c67654594ee92c1b2965a0e081b
Henry Lee <henryleeworld@gmail.com>
2023-11-10 00:07:01 +0800

    feat:升級至 laravel 10

---
+++ lang/vendor/laravel-security-checker/zh_TW/messages.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Security Checker
+    |--------------------------------------------------------------------------
+    |
+    | This file will load all the translations for this package.
+    |
+    */
+
+    'subject_new_vulnerabilities' => '{0} 沒有套件有已知漏洞|{1} 1 個套件有已知漏洞|[2,*] :count 個套件有已知漏洞',
+    'title' => '標題',
+    'view' => '視圖',
+    'cve' => 'CVE',
+    'information' => '資訊',
+    'no_recipients_configured' => '尚未設置收件人！',
+    'body_no_vulnerabilities' => '似乎沒有套件有任何已知的漏洞。您可以閱讀更多有關關閉此訊息 [這裡](https://github.com/jorijn/laravel-security-checker#configuration).'
+];
